b067de11d61aa6e0cadbe7ad1eda2be07ab036a9
Dasari Hari Komal Raj <82033233+Dasari-Hari@users.noreply.github.com>
2022-02-02 14:59:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
 function modelReady(){
-    classifier.classify(gotResults);
-  }
+  classifier.classify(gotResults);
+}
+var dog = 0;
+var cat = 0;
+
   
   function gotResults(error, results) {
     if (error) {
